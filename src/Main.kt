155869dda99e950616931504599a195eed49d117
N-sangwa <nadiinesangwa@gmail.com>
2022-02-17 23:15:13 +0300

    My Assignmnt

---
+++ src/Main.kt
@@ -0,0 +1,13 @@
+fun main() {
+    var name= "Misky Yussuf"
+    var age= "21"
+    var phonenumber= "+254758123456"
+    var weight= "60"
+    var citizenship : Boolean = true
+
+    println(name)
+    println(age)
+    println(phonenumber)
+    println(weight)
+    println(citizenship)
+}
